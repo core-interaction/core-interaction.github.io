ed3aa69d828e8bba94d3d755729fc391d8a7715a
Michael Fehrenbach <mfehrenbach@gmail.com>
2022-08-19 23:27:36 -0400

    Just use the date sorting, oy vey.

--- _plugins/filters.rb
+++ _plugins/filters.rb
@@ -3,6 +3,10 @@ module Jekyll
 		def week_number(input)
 			"Week #{ input.split('.').first }"
 		end
+		def week_date(input)
+			return input if input.to_s.empty?
+			time(input).strftime('%B %-d')
+		end
 	end
 end
 
