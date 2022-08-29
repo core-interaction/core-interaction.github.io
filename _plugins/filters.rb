42b7af728bd55148d34d42099f11ed2148aa67dd
Michael Fehrenbach <mf@michaelfehrenbach.com>
2022-08-29 01:24:27 -0400

    Nix kerning span internals.

--- _plugins/filters.rb
+++ _plugins/filters.rb
@@ -5,41 +5,8 @@ module Jekyll
 			time(input).strftime('%B %-d')
 		end
 		def kerning(input)
-			return input if input.to_s.empty?
-			letters = ""
-			for letter in input.chars do
-				if letter == " "
-					letter_class = "space"
-				elsif letter == "\n"
-					letter_class = false
-				elsif letter == "*"
-					letter_class = false
-				elsif letter == " "
-					letter_class = "nbsp"
-				elsif letter == "-"
-					letter_class = "hyphen"
-				elsif letter == "–"
-					letter_class = "ndash"
-				elsif letter == "—"
-					letter_class = "mdash"
-				elsif letter == ","
-					letter_class = "comma"
-				elsif letter.to_i.to_s == letter
-					letter_class = "num#{letter}"
-				else
-					letter_class = letter
-				end
-				if letter_class
-					letters += "<span class='#{ letter_class }'>#{ letter }</span>"
-				else
-					if letter == "\n"
-						letters += "<br>"
-					else
-						letters += letter
-					end
-				end
-			end
-			return letters
+			# Keeping it here to share Markdown more easily.
+			return input
 		end
 	end
 end
