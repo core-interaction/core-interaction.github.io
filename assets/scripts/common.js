7c135edf2ee158e0f0edc1ddecbf2217c6c6beb3
mfehrenbach <mfehrenbach@users.noreply.github.com>
2022-09-02 15:10:38 +0000

    Reverse `nav` colors on scroll. fac22065cf4dc46be7553f85428b07ffae8a9481

---
+++ assets/scripts/common.js
@@ -0,0 +1,15 @@
+const scrollClass = 'scrolled'
+
+watchScroll = (main) => {
+	const mainTop = main.getBoundingClientRect().top
+
+	const checkMainTop = () => (window.scrollY > mainTop) ? document.body.classList.add(scrollClass) : document.body.classList.remove(scrollClass)
+
+	window.addEventListener('load', checkMainTop)
+	window.addEventListener('resize', checkMainTop)
+	window.addEventListener('scroll', checkMainTop)
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+	watchScroll(document.querySelector('main'))
+})
