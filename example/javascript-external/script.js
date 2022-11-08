3cd1e4030bee14eee139a4fa05a13a386c44b99e
mfehrenbach <mfehrenbach@users.noreply.github.com>
2022-11-08 04:30:26 +0000

    Separate/external examples. ab492220102338096778211599ef321447e52065

---
+++ example/javascript-external/script.js
@@ -0,0 +1,6 @@
+// Set up a variable for our button!
+let button = document.querySelector("#example"); // Any CSS selector.
+
+button.onclick = () => { // Attach the click event.
+	alert("Button was clicked!"); // Pop an alert!
+};
