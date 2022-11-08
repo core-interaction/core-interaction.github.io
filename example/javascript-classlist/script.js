9673f617053e14a510c1fe305e2cdd6841063b80
mfehrenbach <mfehrenbach@users.noreply.github.com>
2022-11-08 05:51:57 +0000

    Forgot one. 6044eba85a76501f312842a2541142f30308f665

--- example/javascript-classlist/script.js
+++ example/javascript-classlist/script.js
@@ -1,4 +1,4 @@
-window.addEventListener('DOMContentLoaded', () => { // Wait for parsing.
+window.addEventListener('DOMContentLoaded', () => { // Wait for parse.
 	let clickedClass = 'clicked'; // Set up variables.
 	let textBlock = document.querySelector('section'); // Any selector.
 	let switchButton = document.querySelector('#example');
